a529271aa9e001c52d62a1e548a513a248f86e57
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-12 01:14:19 +0300

    Create SettingsFragment.java

---
+++ app/src/main/java/com/example/oneui/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.example.myapp; // عدِّل اسم الحزمة بحسب مشروعك
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+
+/**
+ * SettingsFragment: extends PreferenceFragmentCompat to display the settings.
+ * We use One UI SESL PreferenceFragmentCompat to load preferences from XML.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load preferences from the XML resource (res/xml/preferences.xml)
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
